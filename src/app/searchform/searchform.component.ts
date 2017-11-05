fc587e0022529646d7fbab8a343f2d6212c979fc
krzychu124 <krzychu124@gmail.com>
2017-11-05 04:48:39 +0100

    search form request wip

--- src/app/searchform/searchform.component.ts
+++ src/app/searchform/searchform.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {SearchInRadius} from '../SearchInRadius';
-import {NgForm} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
+import { SearchInRadius } from '../SearchInRadius';
+import { NgForm } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { RequestOptions } from '@angular/http';
 import { environment } from '../../environments/environment';
 
 @Component({
@@ -10,21 +11,28 @@ import { environment } from '../../environments/environment';
   styleUrls: ['./searchform.component.css']
 })
 export class SearchformComponent implements OnInit {
- model;
- submitted;
- results: string[];
+  model: SearchInRadius;
+  submitted;
+  results: string[];
   constructor(private http: HttpClient) {
     this.model = new SearchInRadius(0, 0, 0, 'bar');
     this.submitted = false;
   }
   onSubmit(f: NgForm) {
-    this.submitted = true;
     console.log('in onSubmit');
-    console.log(f);
-    this.http.get(environment.serverEndpoint + '/getPlacesInRadius').subscribe(data => {
+    console.log('going to -> url: ' + environment.serverEndpoint + '/getPlacesInRadius');
+    const parameters = new HttpParams()
+      .append('lat', this.model.lat.toString())
+      .append('lng', this.model.lng.toString())
+      .append('radius', this.model.radius.toString())
+      .append('type', this.model.type);
+    const myHeaders = new Headers();
+    myHeaders.set('Content-Type', 'application/json');
+    this.http.get(environment.serverEndpoint + '/getPlacesInRadius', { params: parameters }).subscribe(data => {
       // Read the result field from the JSON response.
       this.results = data['results'];
-      console.log('retrieved!');
+      console.log(data);
+      this.submitted = true;
     });
   }
 
