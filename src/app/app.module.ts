6a4ed15805fd5f52bd03cbc936cba9c1b960b508
krzychu124 <krzychu124@gmail.com>
2017-11-05 04:43:06 +0100

    home component + basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchformComponent } from './searchform/searchform.component';
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { MapComponent } from './map/map.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SearchformComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'map', component: MapComponent }]),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAULrtLKbtbVG5vBESqFyJTqE0F_Ykb7xs'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  notSupported: string;
+  constructor(private _route: Router) {
+      this.notSupported = '';
+  }
+
+  ngOnInit() {
+  }
+
+  login() {
+      this._route.navigate(['/login']);
+  }
+
+  map() {
+      this._route.navigate(['/map']);
+  }
+}
