5a34c3ee261e889f7b73bc7d88b2b4ffd854eb42
krzychu124 <krzychu124@gmail.com>
2017-11-12 16:03:35 +0100

    home component improvements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  notSupported: string;
-  constructor(private _route: Router) {
-      this.notSupported = '';
-  }
+    notSupported: string;
+    constructor(private _route: Router, private _auth: AuthService) {
+        this.notSupported = '';
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  login() {
-      this._route.navigate(['/login']);
-  }
+    login() {
+        this._route.navigate(['/login']);
+    }
 
-  map() {
-      this._route.navigate(['/map']);
-  }
+    logout() {
+        this._auth.logout();
+    }
+    isLoggedIn() {
+        return this._auth.isAuthenticated();
+    }
 
-  register() {
-      this._route.navigate(['/register']);
-  }
+    map() {
+        this._route.navigate(['/map']);
+    }
+
+    register() {
+        this._route.navigate(['/register']);
+    }
 }
