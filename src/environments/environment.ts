896c60bf83962715b413fc3d7cd2f613f04c452c
krzychu124 <krzychu124@gmail.com>
2017-11-12 06:27:18 +0100

    updated server endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,55 @@
 
 export const environment = {
   production: false,
-  serverEndpoint: 'http://localhost:8088/'
+  serverEndpoint: 'http://localhost:8088/',
+  /**
+   * Get all restaurants in radius
+   * Required rarameters: lat, lng, radius: number
+   */
+  placesInRadiusEndpoint: '/places/area', // /places?lat=&lng=&radius&type=
+  /**
+   * Get place details. Complete endpoint: '/places/{placeId}/details'
+   */
+  placesDetailsEndpoint: '/details', // /{placeId}/details
+  /**
+   * Get all ingredients
+   */
+  ingredientEndopoint: '/ingredients',
+  /**
+   * Post new ingredient type { 'id': '', 'name': ''}
+   */
+  ingredientAddEndpoint: '/ingredients/add',
+  /**
+   * Get restaurant ingredient ratings where ingredient /rating/{ingredientName}
+   */
+  ratingEndpoint: '/rating/', //  - string
+  /**
+   * Post NewUser credentials { username: '', password: ''} // more in the future...
+   */
+  registerEndpoint: '/register',
+  /**
+   * Post User credentials { username: '', password: ''}
+   */
+  registerToApiEndpoint: '/api/register', // in-development
+  /**
+   * Post Updated restaurant information (PlaceDetailsData)
+   * {
+   *  'id': 'string',
+   * 'name': 'string',
+   * 'location': [ 'lat': '', 'lng': ''],
+   * 'newlyCreated':'boolean',
+   * 'foodTypes': [ ],
+   * 'ingredientRatings': [ ]
+   * }
+   */
+  restaurantSaveEndpoint: '/restaurants/save',
+  /**
+   * Get restaurant id info. Use '/restaurant/{restaurant/placeId}'
+   */
+  restaurantEndpoint: '/restaurants/',
+  /**
+   * WiP!
+   * Get restaurants by foodType. Use '/restaurant/type/{foodType}'
+   */
+  restaurantByFoodEndpoint: '/restaurant/type/'
 };
