fbbda0755420a3f981d5581b08c03df50e276b12
krzychu124 <krzychu124@gmail.com>
2017-11-12 18:46:56 +0100

    fix token null poiner exception

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -12,15 +12,17 @@ export class TokenInterceptor implements HttpInterceptor {
   constructor() { }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (request.method === 'GET' || request.method === 'POST') {
-     const sessionToken =  JSON.parse(sessionStorage.getItem('token')) as SessionToken;
-      const token = sessionToken.token;
-      if (token !== undefined && token !== null) {
-        request = request.clone({
-          setHeaders: {
-            'X-XSRF-Token': token
-          }
-        });
-        return next.handle(request);
+      const sessionToken = JSON.parse(sessionStorage.getItem('token')) as SessionToken;
+      if (sessionToken !== undefined && sessionToken != null) {
+        const token = sessionToken.token;
+        if (token !== undefined && token !== null) {
+          request = request.clone({
+            setHeaders: {
+              'X-XSRF-Token': token
+            }
+          });
+          return next.handle(request);
+        }
       }
     }
     return next.handle(request);
