47bc5b82610a0f7734a28b57bb2a7f8e6301c7e5
krzychu124 <krzychu124@gmail.com>
2017-11-13 03:20:55 +0100

    minor changes

--- src/app/shared/response-info.ts
+++ src/app/shared/response-info.ts
@@ -1,8 +1,13 @@
 export class ResponseInfo {
     public key: string;
-    public code: 0;
+    public code: number;
     public desc: string;
     public object: object;
+    constructor() {
+        this.key = '';
+        this.code = -1;
+        this.desc = '';
+    }
     public stringInfo(): string {
         return 'key: ' + this.key + ' code: ' + this.code + ' desc:' + this.desc + ' obj: ' + this.object;
     }
