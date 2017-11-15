9a82ba997b871810e93dad66603bfc792bd01c19
krzychu124 <krzychu124@gmail.com>
2017-11-15 17:16:17 +0100

    ingredient rating fix

--- src/app/shared/ingredient-rating.ts
+++ src/app/shared/ingredient-rating.ts
@@ -3,13 +3,11 @@ import { IngredientType } from './ingredient-type';
 import { Thumb } from './thumb';
 
 export class IngredientRating {
-    id: number;
     ingredient: IngredientType;
     restaurant: PlaceDetailsData;
     thumb: Thumb;
 
     constructor() {
-        this.id = 0;
         this.ingredient = new IngredientType;
         this.restaurant = new PlaceDetailsData;
         this.thumb = new Thumb();
