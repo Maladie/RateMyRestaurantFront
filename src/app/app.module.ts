13b1e2b34b07dac5177573703267e34718457ee9
krzychu124 <krzychu124@gmail.com>
2017-12-01 05:28:10 +0100

    angular material2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,17 @@ import { EqualityValidatorDirective } from './shared/equality-validator.directiv
 import { PlaceDetailCardComponent } from './map/place-detail-card/place-detail-card.component';
 import { IngredientRating } from './shared/ingredient-rating';
 import { VotingLockService } from './shared/voting-lock.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatAutocompleteModule, MatAutocomplete } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material';
+import { FoodSearchComponent } from './map/food-search/food-search.component';
+import { DataService } from './shared/data.service';
 
 @NgModule({
   declarations: [
@@ -34,6 +45,7 @@ import { VotingLockService } from './shared/voting-lock.service';
     PlaceDetailsComponent,
     EqualityValidatorDirective,
     PlaceDetailCardComponent,
+    FoodSearchComponent
   ],
   imports: [
     BrowserModule,
@@ -44,7 +56,16 @@ import { VotingLockService } from './shared/voting-lock.service';
     AgmSnazzyInfoWindowModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAULrtLKbtbVG5vBESqFyJTqE0F_Ykb7xs'
-    })
+    }),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatCardModule,
+    MatAutocompleteModule,
+    MatInputModule
   ],
   providers: [{
     provide: AuthService,
@@ -59,11 +80,15 @@ import { VotingLockService } from './shared/voting-lock.service';
     provide: VotingLockService,
     useClass: VotingLockService
   },
-{
-  provide: HTTP_INTERCEPTORS,
-  useClass: TokenInterceptor,
-  multi: true
-}],
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi: true
+  },
+  {
+    provide: DataService,
+    useClass: DataService
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
