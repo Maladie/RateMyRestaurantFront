36f08be5ffcffa5774da25202a2e456f89a88b42
krzychu124 <krzychu124@gmail.com>
2017-12-05 03:49:18 +0100

    fixed error info, converted to dialog component, added register dialog show action

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginData } from './login-data';
 import { AuthService } from '../shared/auth.service';
@@ -6,7 +6,8 @@ import { ResponseInfo } from '../shared/response-info';
 import { Observable } from 'rxjs/Observable';
 import { Location } from '@angular/common';
 import { VotingLockService } from '../shared/voting-lock.service';
-
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,8 @@ import { VotingLockService } from '../shared/voting-lock.service';
 export class LoginComponent implements OnInit {
   model: LoginData;
   result: ResponseInfo;
-  constructor(private _location: Location, private _auth: AuthService) {
+  processingAnim = false;
+  constructor(private _location: Location, private _auth: AuthService, public dialogRef: MatDialogRef<LoginComponent>, private dialog: MatDialog) {
     this.model = new LoginData('', '');
     this.result = new ResponseInfo();
   }
@@ -25,16 +27,26 @@ export class LoginComponent implements OnInit {
   }
 
   submit() {
-    this._auth.loginUser(this.model).then(ok => {
-      this.result = ok as ResponseInfo;
-      // temporary
-      sessionStorage.setItem('username', this.model.username);
-      this._location.back();
-    }, nok =>{
-      console.log('nok ' +nok);
+    this.processingAnim = true; // show animation
+    this.result.code = -1; // reset before response + hide error label
+    this._auth.loginUser(this.model).subscribe(ok => {
+      this.processingAnim = false; // hide animation
+      this.dialogRef.close();
+    }, nok => {
+      this.processingAnim = false; // hide animation
+      console.log('nok ' + nok);
       this.result = nok.error as ResponseInfo;
-      // temporary
-      sessionStorage.removeItem('username');
+    });
+  }
+
+  register() {
+    // dialogs chain login -> register(if register success and autologin:true => new user details with response) -> login
+    const result = this.dialog.open(RegisterComponent, { width: '400px', data: { username: null, password: null } });
+    result.afterClosed().subscribe(resp => {
+      // close this dialog if autologin successfully logged-in user
+      if (this._auth.isAuthenticated()) {
+        this.dialogRef.close();
+      }
     });
   }
 }
